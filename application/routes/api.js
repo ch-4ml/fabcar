173de6749b89be9efcdd4c63be9313f09e408783
ch-4ml <fetch@ajou.ac.kr>
2020-11-17 15:17:02 +0900

    ~router

---
+++ application/routes/api.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+const { FileSystemWallet, Gateway } = require('fabric-network');
+const fs = require('fs');
+const path = require('path');
+
+const ccpPath = path.resolve(__dirname, '..', '..', 'basic-network', 'connection.json');
+const ccpJSON = fs.readFileSync(ccpPath, 'utf8');
+const ccp = JSON.parse(ccpJSON);
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/api/cars', async (req, res) => {
+  const result = await callChainCode('queryAllCars', false);
+  res.json(JSON.parse(result));
+});
+
+router.get('/api/cars/:carNo', async (req, res) => {
+  const carNo = req.params.carNo;
+  const result = await callChainCode('queryCar', false, carNo);
+  res.json(JSON.parse(result));
+});
+
+router.post('/api/cars', async (req, res) => {
+  const args = [req.body.carNo, req.body.make, req.body.model, req.body.colour, req.body.owner];
+  await callChainCode('createCar', true, args);
+
+});
+
+router.put('/api/cars/:carNo', async (req, res) => {
+  const args = [req.params.carNo, req.body.carOwner];
+  await callChainCode('changeCarOwner', args);
+});
+
+async function callChainCode(fnName, isSubmit, ...args) {
+  try {
+    // Create a new file system based wallet for managing identities.
+    const walletPath = path.join(process.cwd(), 'wallet');
+    const wallet = new FileSystemWallet(walletPath);
+    console.log(`Wallet path: ${walletPath}`);
+
+    // Check to see if we've already enrolled the user.
+    const userExists = await wallet.exists('user1');
+    if (!userExists) {
+        console.log('An identity for the user "user1" does not exist in the wallet');
+        console.log('Run the registerUser.js application before retrying');
+        return;
+    }
+
+    // Create a new gateway for connecting to our peer node.
+    const gateway = new Gateway();
+    await gateway.connect(ccp, { wallet, identity: 'user1', discovery: { enabled: false } });
+
+    // Get the network (channel) our contract is deployed to.
+    const network = await gateway.getNetwork('mychannel');
+
+    // Get the contract from the network.
+    const contract = network.getContract('fabcar');
+
+    let result;
+    if(isSubmit) {
+      result = await contract.submitTransaction(fnName, ...args);
+      console.log(`Transaction has been submitted. result: ${result.toString()}`);
+    } else {
+      result = await contract.evaluateTransaction(fnName, ...args);
+      console.log(`Transaction has been evaluated. result: ${result.toString()}`);
+    }
+    return result;
+
+  } catch(err) {
+    console.error(`Failed to evaluate transaction: ${error}`);
+    process.exit(1);
+  }
+}
+
+module.exports = router;
